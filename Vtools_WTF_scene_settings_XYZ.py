693658eff13e63df585a3a1e15fa1e0276b8fb9b
Vaclav Benc <vaclav.benc@gmail.com>
2018-10-12 13:59:30 +0200

    Forgotten print cleanup.

--- Vtools_WTF_scene_settings_XYZ.py
+++ Vtools_WTF_scene_settings_XYZ.py
@@ -112,7 +112,6 @@ class WTF_scene_settings_XYZ(bpy.types.Operator):
       for renderlayer_name in renderlayer_list:
         render_layer = scene.render.layers[renderlayer_name]
         bpy.ops.scene.duplicate_render_layer(set_appendix = '-Normalmap', set_source_render_layer = render_layer.name, set_source_scene = scene.name, set_target_scene = scene.name)
-        print(scene.name, 'is XJXJFJIFY SCENE NAME')
         scene.render.layers[renderlayer_name+'-Normalmap'].material_override = bpy.data.materials['Normalmap']
 
 
