56bc3ea9ccb545ce1ad85c21ba991b6a500332a2
Vaclav Benc <vaclav.benc@factorio.com>
2019-01-07 17:59:18 +0100

    Version 0.3.6

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'author': 'V453000',
     'description': 'Various tools.',
     'category': 'User',
-    'version': (0, 3, 5),
+    'version': (0, 3, 6),
     'blender': (2, 7, 9)
 }
 
